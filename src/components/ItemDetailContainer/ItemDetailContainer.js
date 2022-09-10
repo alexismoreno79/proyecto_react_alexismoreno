e24fcb4551f96d7eae64dbbe702fed103e40e674
Alexis Moreno <alexisdariomoreno@gmail.com>
2022-09-09 22:18:45 -0300

    clase de firebase 1

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,9 +1,11 @@
 // Dentro de ItemDeatilContainer.js
 import './ItemDetailContainer.css'
 import { useState, useEffect } from 'react'
-import { getProductById } from '../../asyncMock'
+// import { getProductById } from '../../asyncMock'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
+import { getDoc, doc } from 'firebase/firestore'
+import { db } from '../../services/firebase'
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState()
@@ -12,11 +14,17 @@ const ItemDetailContainer = () => {
     const { productId } = useParams()
 
     useEffect(() => {
-        getProductById(productId).then(response => {
-            setProduct(response)
+
+        getDoc(doc(db, 'products', productId)).then(response => {
+            const data = response.data()
+            const productAdapted = { id: response.id, ...data}
+            setProduct(productAdapted)
+        }).catch(error => {
+            console.log(error)
         }).finally(() => {
             setLoading(false)
         })
+
     }, [productId])
 
     return(
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,36 +1,37 @@
 // Dentro de ItemListContainer.js
 import './ItemListContainer.css'
 import { useState, useEffect } from 'react'
-import { getProducts, getProductsByCategory } from "../../asyncMock"
+// import { getProducts, getProductsByCategory } from "../../asyncMock"
 import ItemList from '../ItemList/ItemList'
 import { useParams } from 'react-router-dom' 
+import { getDocs, collection, query, where } from 'firebase/firestore'
+import { db } from '../../services/firebase'
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
-
-    const [title, setTitle] = useState('primer titulo')
-
     const { categoryId } = useParams()
 
     useEffect(() => {
         setLoading(true)
-        const asyncFunction = categoryId ? getProductsByCategory : getProducts
-        
-        asyncFunction(categoryId).then(response => {
-            setProducts(response)
+
+        const collectionRef = !categoryId 
+            ? collection(db, 'products')
+            : query(collection(db, 'products'), where('category', '==', categoryId))
+
+        getDocs(collectionRef).then(response => {
+            const productsAdapted = response.docs.map(doc => {
+                const data = doc.data()
+                return { id: doc.id, ...data}
+            })
+            setProducts(productsAdapted)
         }).catch(error => {
             console.log(error)
         }).finally(() => {
             setLoading(false)
-        })  
+        })
     }, [categoryId])
 
-    useEffect(() => {
-        setTimeout(() => {
-            setTitle('segundo titulo')
-        }, 3000)
-    })
 
     if(loading) {
         return <h1>Cargando productos...</h1>
@@ -39,7 +40,6 @@ const ItemListContainer = ({ greeting }) => {
     return (
         <div onClick={() => console.log('click en itemlistcontainer')}>
             <h1>{`${greeting} ${categoryId || ''}`}</h1>
-            <h2>{title}</h2>
             <ItemList products={products} />
         </div>
     )
